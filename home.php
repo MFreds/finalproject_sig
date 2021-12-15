08414fa13124a21cbb639ad2824414a86e2d17c2
MFredS <sinagafredy26@gmail.com>
2021-12-15 15:49:14 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,6 @@
+<div class="page-header">
+    <h1>Sistem Informasi Geografis</h1>
+</div>
+<p>Halaman Utama, halaman utama akan menampilkan peta besar berisikan tempat berupa marker yang bisa diklik dan menampilkan detail tempat, baik deskripsi, galeri, peta, dan rute.</p>
+<p>Tempat, pengolahan tempat yang dinamis, dengan google map lokasi bisa diposisikan dengan drag and drop.</p>
+<p>Login & Password, untuk bisa mengolah data tempat, kita harus login ke sistem informasi geografis.</p>
